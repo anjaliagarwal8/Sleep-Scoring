24cb115bbd1c033d480127cddabf32b0290ad628
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-08-09 19:57:36 +0530

    completed draw_HMC_sampes function

--- mcRBM/draw_HMC_samples.m
+++ mcRBM/draw_HMC_samples.m
@@ -1,10 +1,7 @@
-function [hmc_step, hmc_ave_rej] = draw_HMC_samples(data,negdata,normdata,VF,FH,hb_cov,vb,W,hb_mean,hmc_step,hmc_step_nr,hmc_ave_rej,hmc_target_ave_rej,t1,t2,t3,t4,t5,t6,t7,thresh,feat,featsq,batch_size,feat_mean,len,lengthsq,normcoeff,small,num_vis)
+function [hmc_step, hmc_ave_rej] = draw_HMC_samples(data,VF,FH,hb_cov,vb,W,hb_mean,hmc_step,hmc_step_nr,hmc_ave_rej,hmc_target_ave_rej,batch_size,small,num_vis)
 
     vel = randn(size(data));
     negdata = data;
-    old_energy = zeros(size(data,2));
-    gradient = 0;
-    normgradient = 
     
     [old_energy,vel] = compute_energy_mcRBM(negdata,vel,VF,FH,hb_cov,vb,W,hb_mean,small);
     gradient = compute_gradient_mcRBM(negdata,VF,FH,hb_cov,vb,W,hb_mean,small);
@@ -34,5 +31,27 @@ function [hmc_step, hmc_ave_rej] = draw_HMC_samples(data,negdata,normdata,VF,FH,
     %Rejection
     thresh = exp(old_energy - new_energy);
     t4 = rand(1,batch_size);
+    t4 = t4 < thresh;
+    
+    % Update negdata and rejection rate
+    t4 = t4*(-1);
+    t4 = t4 +  1;
+    t5 = sum(t4,2);
+    rej = t5(1,1)/batch_size;
+    t6  = zeros(num_vis,batch_size);
+    for i=1:size(t6,1)
+            t6(i,:) = data(i,:) .* t4; % Multiply by row
+    end
+    t7  = zeros(num_vis,batch_size);
+    for i=1:size(t7,1)
+            t7(i,:) = negdata(i,:) .* t4; % Multiply by row
+    end
+    negdata = negdata - t7 + t6;
+    hmc_ave_rej = 0.9*hmc_ave_rej + 0.1*rej;
+    if hmc_ave_rej < hmc_target_ave_rej
+        hmc_step = min(hmc_step*1.01,0.25);
+    else
+        hmc_step = max(hmc_step*0.99,.001);
+    end
     
 end
