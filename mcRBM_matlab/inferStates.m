22a52c496690d80c91ea51f900ff6cd4863c9879
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-09-07 19:51:28 +0530

    infer states 2

--- mcRBM_matlab/inferStates.m
+++ mcRBM_matlab/inferStates.m
@@ -1,3 +1,4 @@
+% Loading data and variables 
 visData = load('visData_final.mat');
 variables = load('variables_final.mat');
 
@@ -13,3 +14,19 @@ vb = variables.vb;
 hb_cov = variables.hb_cov;
 hb_mean = variables.hb_mean;
 
+% Compute the probabilities of the covariance units
+%normalize data for covariance hidden
+dsq = np.square(self.d)
+		lsq = np.sum(dsq, axis=0)
+		lsq /= self.d.shape[1]
+		lsq += np.spacing(1)
+		l = np.sqrt(lsq)
+		normD = self.d/l
+		
+		# compute logistic_covarinace_argument:	
+		logisticArg_c = (-0.5*np.dot(FH.T, np.square(np.dot(VF.T, normD.T))) + bias_cov).T
+        
+dsq = d.^2;
+lsq = sum(dsq);
+lsq = lsq./size(d,2);
+lsq = lsq + eps(1);
