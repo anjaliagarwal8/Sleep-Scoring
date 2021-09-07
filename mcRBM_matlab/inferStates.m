00999887b2df19ccb54590940bf514851d93d1e2
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-09-07 20:03:48 +0530

    infer states 3

--- mcRBM_matlab/inferStates.m
+++ mcRBM_matlab/inferStates.m
@@ -1,4 +1,4 @@
-% Loading data and variables 
+%% Loading data and variables 
 visData = load('visData_final.mat');
 variables = load('variables_final.mat');
 
@@ -14,19 +14,27 @@ vb = variables.vb;
 hb_cov = variables.hb_cov;
 hb_mean = variables.hb_mean;
 
-% Compute the probabilities of the covariance units
-%normalize data for covariance hidden
-dsq = np.square(self.d)
-		lsq = np.sum(dsq, axis=0)
-		lsq /= self.d.shape[1]
-		lsq += np.spacing(1)
-		l = np.sqrt(lsq)
-		normD = self.d/l
-		
-		# compute logistic_covarinace_argument:	
-		logisticArg_c = (-0.5*np.dot(FH.T, np.square(np.dot(VF.T, normD.T))) + bias_cov).T
+%% Compute latent activations
+% Compute the probabilities of the covariance units (normalize data for
+% covariance hidden)
         
 dsq = d.^2;
 lsq = sum(dsq);
 lsq = lsq./size(d,2);
 lsq = lsq + eps(1);
+l = sqrt(lsq);
+normD = d./l;
+
+% Compute logistic_covariance_argument
+logisticArg_c = (-0.5*FH'*((VF'*normD').^2) + hb_cov)';
+
+% compute hidden_covariance probabilities:
+p_hc = sigmoid(logisticArg_c);
+
+% compute logistic_mean_argument (use unnormalised data for mean hidden)
+logisticArg_m = d*W + hb_mean';
+
+% compute hidden_mean probabilities
+p_hm = sigmoid(logisticArg_m);
+
+%% Infer latent states from latent activations
