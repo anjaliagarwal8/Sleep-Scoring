1d041d4da1aa668d9deeeb163ea44561e68aca69
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-11-09 19:01:24 +0530

    Update statesHistogram.m

--- mcRBM_matlab/analysis/statesHistogram.m
+++ mcRBM_matlab/analysis/statesHistogram.m
@@ -1,3 +1,19 @@
-%% Method plotting the histogram of the latent states, i.o.w. how many samples 
-% fall in each unique latent state.
+%% Method computing the histogram over the desired latent states &
+% reordering it for visualization.
 
+load uniqueStates.mat
+load obsKeys.mat
+load stageDistributionMat.mat
+
+[status, msg, msgID] = mkdir('statesHistogram');
+cd statesHistogram
+
+latentStates = length(uniqueStates);
+
+% Compute histogram over latent states of interest
+centroidsHist = zeros(latentStates,3);
+for s=1:latentStates
+    centroidsHist(s,1) = uniqueStates(s,2);
+    centroidsHist(s,2) = uniqueStates(s,2);
+    centroidsHist(s,3) = s;
+end
