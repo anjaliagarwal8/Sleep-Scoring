eb58c41fa38ed612f1bcbe302304236aeb08ac9d
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-11-15 20:56:17 +0530

    Update analyzeStates.m

--- mcRBM_matlab/analysis/analyzeStates.m
+++ mcRBM_matlab/analysis/analyzeStates.m
@@ -2,3 +2,11 @@ load data.mat
 load uniqueStates.mat
 load obsKeys.mat
 
+powerband_features = ['delta_theta','delta_alpha','delta_beta','delta_gamma','theta_alpha','theta_beta',
+    'theta_gamma','alpha_beta','alpha_gamma','beta_gamma'];
+band_range = [floor(min(d(:,1:10))); ceil(max(d(:,1:10)))];
+emg_range = [floor(min(d(:,11))); ceil(max(d(:,11)))];
+
+for l=1:length(uniqueStates)
+    
+end
