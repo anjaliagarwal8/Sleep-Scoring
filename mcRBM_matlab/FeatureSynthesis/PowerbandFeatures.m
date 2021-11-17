0818ba548a710f1271fc69d4a34911ba0793b5ea
Anjali Agarwal <anjaliagrl8@gmail.com>
2021-11-17 19:49:54 +0530

    Update PowerbandFeatures.m

--- mcRBM_matlab/FeatureSynthesis/PowerbandFeatures.m
+++ mcRBM_matlab/FeatureSynthesis/PowerbandFeatures.m
@@ -1,14 +1,20 @@
 %% Synthesis of power band features for each band from the raw sleep dataset
 
-% Frequency ranges for each band 
+% Frequency ranges for each band
+f_all = [0.1 24];
 f_delta = [0.5 4];
 f_theta = [6 12];
 f_beta = [12 23];
 
+% Converting into log space
+numfreqs = 100;
+FFTfreqs = logspace(log10(f_all(1)),log10(f_all(2)),numfreqs);
+
 Fs = % sampling rate
 window = 2;
 noverlap = 0;
-[thFFTspec,thFFTfreqs,t_FFT] = spectrogram(data,window*Fs,noverlap*Fs,thFFTfreqs,Fs);
+
+[thFFTspec,thFFTfreqs,t_FFT] = spectrogram(data,window*Fs,noverlap*Fs,FFTfreqs,Fs);
 thFFTspec = (abs(thFFTspec));
 [zFFTspec,mu,sig] = zscore(log10(thFFTspec)');
 thfreqs = find(thFFTfreqs>=f_theta(1) & thFFTfreqs<=f_theta(2));
