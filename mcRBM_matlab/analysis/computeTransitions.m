72cccb009337113f26c5f4a08e950591625fbc41
Anjali Agarwal <anjaliagrl8@gmail.com>
2022-01-04 19:57:23 +0530

    Update computeTransitions.m

--- mcRBM_matlab/analysis/computeTransitions.m
+++ mcRBM_matlab/analysis/computeTransitions.m
@@ -1,18 +1,18 @@
 %% Script written for finding the transition probability of each latent state
 
 % Getting the obsKeys data from the inferStates script
-load obsKeys.mat
+load inferredStates.mat
 load uniqueStates.mat
 
 countTransMat = zeros(size(uniqueStates,1),size(uniqueStates,1));
 
 %% Computing transition probability matrix
-for i=1:length(obsKeys)-1
-    if obsKeys(i+1,2) - obsKeys(i,2) == 1
-        a = obsKeys(i,1);
-        b = obsKeys(i+1,1);
+for i=1:length(states)-1
+    %if states(i+1,2) - states(i,2) == 1
+        a = states(i,1);
+        b = states(i+1,1);
         countTransMat(a,b) = countTransMat(a,b) + 1;
-    end
+    %end
 end
 
 [status, msg, msgID] = mkdir('transMat');
@@ -40,9 +40,9 @@ save transitionMatThresholded.mat thresholdTransMat
 G = digraph(countTransMat);
 plot(G)
 
-mc = dtmc(transMat);
+mc = dtmc(transMat,'StateNames',["3","1/3","3","3","3","1","3","3","3","3","3/5"]);
 figure;
 graphplot(mc);
-
+saveas(gcf,'markovgraph.png')
 %% HeatMap
 imagesc(countTransMat)
