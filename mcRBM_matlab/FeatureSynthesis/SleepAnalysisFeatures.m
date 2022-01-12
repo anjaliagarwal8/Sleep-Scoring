0e6858b1330eafea12481897fe3782c89d9f58e1
Anjali Agarwal <anjaliagrl8@gmail.com>
2022-01-12 13:49:10 +0530

    Update SleepAnalysisFeatures.m

--- mcRBM_matlab/FeatureSynthesis/SleepAnalysisFeatures.m
+++ mcRBM_matlab/FeatureSynthesis/SleepAnalysisFeatures.m
@@ -1,8 +1,8 @@
 %% Synthesis of power band features for each band from the raw sleep dataset
 
 clear; close all; clc
-[DataPFC, TimeVectLFP, HeadingData] = load_open_ephys_data_faster('100_CH2_0.continuous');
-[DataHPC, ~, ~] = load_open_ephys_data_faster('100_CH53_0.continuous');
+[DataHPC, TimeVectLFP, HeadingData] = load_open_ephys_data_faster('100_CH2_0.continuous');
+[DataPFC, ~, ~] = load_open_ephys_data_faster('100_CH53_0.continuous');
 % extracting the sampling frequency of the data
 SamplingFreq = HeadingData.header.sampleRate;       % Sampling frequency of the data
 % Downsample the data to different sampling rates for fast processing
@@ -13,7 +13,7 @@ lfpHPCDown = decimate(DataHPC,timesDownSamp1,'FIR');
 timVect1 = linspace(0,numel(lfpPFCDown)/TargetSampling1,numel(lfpPFCDown));
 
 % Creating epochs for assigning it to various substates later
-epochTimVec = 3.1; %s
+epochTimVec = 1; %s
 epochSampLen = epochTimVec*TargetSampling1;
 numEpochs = floor(length(lfpHPCDown)/epochSampLen);
 
@@ -30,14 +30,14 @@ FFTfreqs = logspace(log10(f_all(1)),log10(f_all(2)),numfreqs);
 % Parameters for getting the spectrogram
 Fs = TargetSampling1; % sampling rate
 window = 2; 
-noverlap = 0;
+noverlap = window-1;
 
 % parameteres for EMG-like signal
 samplingFrequencyEMG = 5;
 smoothWindowEMG = 10;
 matfilename = 'EMGLikeSignalMat';
 
-lfpFeatures = zeros(numEpochs,7);
+lfpFeatures = zeros(numEpochs,4);
 for i=1:numEpochs
     lfpPFCEpoch = lfpPFCDown((i-1)*epochSampLen+1:i*epochSampLen);
     lfpHPCEpoch = lfpHPCDown((i-1)*epochSampLen+1:i*epochSampLen);
@@ -50,46 +50,46 @@ for i=1:numEpochs
     delfreqs = f_FFT>=f_delta(1) & f_FFT<=f_delta(2);
     delpower = sum((FFTspec(delfreqs,:)),1);
 
-    % theta power
-    thfreqs = f_FFT>=f_theta(1) & f_FFT<=f_theta(2);
-    thpower = sum((FFTspec(thfreqs,:)),1);
+%     % theta power
+%     thfreqs = f_FFT>=f_theta(1) & f_FFT<=f_theta(2);
+%     thpower = sum((FFTspec(thfreqs,:)),1);
     
     % beta power
     betafreqs = f_FFT>=f_beta(1) & f_FFT<=f_beta(2);
     betapower = sum((FFTspec(betafreqs,:)),1);
    
-    % Calculating the ratios....
-    lfpFeatures(i,1) = delpower/thpower;
-    lfpFeatures(i,2) = delpower/betapower;
-    lfpFeatures(i,3) = thpower/betapower;
+%     % Calculating the ratios....
+%     lfpFeatures(i,1) = delpower/thpower;
+%     lfpFeatures(i,2) = delpower/betapower;
+%     lfpFeatures(i,3) = thpower/betapower;
 
     % 
     [FFTspec,f_FFT,t_FFT] = spectrogram(lfpHPCEpoch,window*Fs,noverlap*Fs,FFTfreqs,Fs);
     FFTspec = (abs(FFTspec));
 
-    % delta power
-    delfreqs = find(f_FFT>=f_delta(1) & f_FFT<=f_delta(2));
-    delpower = sum((FFTspec(delfreqs,:)),1);
+%     % delta power
+%     delfreqs = find(f_FFT>=f_delta(1) & f_FFT<=f_delta(2));
+%     delpower = sum((FFTspec(delfreqs,:)),1);
 
     % theta power
     thfreqs = find(f_FFT>=f_theta(1) & f_FFT<=f_theta(2));
     thpower = sum((FFTspec(thfreqs,:)),1);
     
-    % beta power
-    betafreqs = find(f_FFT>=f_beta(1) & f_FFT<=f_beta(2));
-    betapower = sum((FFTspec(betafreqs,:)),1);
+%     % beta power
+%     betafreqs = find(f_FFT>=f_beta(1) & f_FFT<=f_beta(2));
+%     betapower = sum((FFTspec(betafreqs,:)),1);
     
     % Calculating the ratios....
-    lfpFeatures(i,4) = delpower/thpower;
-    lfpFeatures(i,5) = delpower/betapower;
-    lfpFeatures(i,6) = thpower/betapower;
+    lfpFeatures(i,1) = delpower/thpower;
+    lfpFeatures(i,2) = delpower/betapower;
+    lfpFeatures(i,3) = thpower/betapower;
 
     % Calculating EMG-like signal
     EMGFromLFP = compute_emg_buzsakiMethod(samplingFrequencyEMG, Fs, lfpPFCEpoch, lfpHPCEpoch, smoothWindowEMG,matfilename);
     % Integrating the EMG signal for each epoch
     EMGSig = trapz(EMGFromLFP.smoothed);
 
-    lfpFeatures(i,7) = EMGSig;
+    lfpFeatures(i,4) = EMGSig;
 end
 
 save Features.mat lfpFeatures
@@ -105,16 +105,14 @@ States = load('2019-06-06_13-26-20_Post-Trial3-states.mat');
 downsampledStates = downsample(States.states,25);
 downsampledStates(1,110) = 1; 
 
-state_0 = downsampledStates == 0;
-state_1 = downsampledStates == 1;
-state_2 = downsampledStates == 2;
-state_3 = downsampledStates == 3;
-state_4 = downsampledStates == 4;
-state_5 = downsampledStates == 5;
+save states.mat downsampledStates
 
-Features_0 = PreprocessedFeatures(state_0,:);
+state_1 = downsampledStates == 1; %Wake
+state_3 = downsampledStates == 3; %NREM
+state_4 = downsampledStates == 4; %NREM-REM
+state_5 = downsampledStates == 5; %REM
+
 Features_1 = PreprocessedFeatures(state_1,:);
-Features_2 = PreprocessedFeatures(state_2,:);
 Features_3 = PreprocessedFeatures(state_3,:);
 Features_4 = PreprocessedFeatures(state_4,:);
 Features_5 = PreprocessedFeatures(state_5,:);
@@ -122,79 +120,20 @@ Features_5 = PreprocessedFeatures(state_5,:);
 [status, msg, msgID] = mkdir('FeatureHistograms');
 cd FeatureHistograms
 
-histogram(Features_1(:,1))
-hold on
-histogram(Features_3(:,1))
-hold on
-histogram(Features_4(:,1))
-hold on
-histogram(Features_5(:,1))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch2__del__theta')
-saveas(gcf,'Ch2_del_theta.png')
-
-histogram(Features_1(:,2))
-hold on
-histogram(Features_3(:,2))
-hold on
-histogram(Features_4(:,2))
-hold on
-histogram(Features_5(:,2))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch2__del__beta')
-saveas(gcf,'Ch2_del_beta.png')
-
-histogram(Features_1(:,3))
-hold on
-histogram(Features_3(:,3))
-hold on
-histogram(Features_4(:,3))
-hold on
-histogram(Features_5(:,3))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch2__theta__beta')
-saveas(gcf,'Ch2_theta_beta.png')
-
-histogram(Features_1(:,4))
-hold on
-histogram(Features_3(:,4))
-hold on
-histogram(Features_4(:,4))
-hold on
-histogram(Features_5(:,4))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch53__del__theta')
-saveas(gcf,'Ch53_del_theta.png')
-
-histogram(Features_1(:,5))
-hold on
-histogram(Features_3(:,5))
-hold on
-histogram(Features_4(:,5))
-hold on
-histogram(Features_5(:,5))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch53__del__beta')
-saveas(gcf,'Ch53_del_beta.png')
-
-histogram(Features_1(:,6))
-hold on
-histogram(Features_3(:,6))
-hold on
-histogram(Features_4(:,6))
-hold on
-histogram(Features_5(:,6))
-legend('WAKE','NREM','NREM-REM','REM')
-title('Ch53__theta__beta')
-saveas(gcf,'Ch53_theta_beta.png')
-
-histogram(Features_1(:,7))
-hold on
-histogram(Features_3(:,7))
-hold on
-histogram(Features_4(:,7))
-hold on
-histogram(Features_5(:,7))
-legend('WAKE','NREM','NREM-REM','REM')
-title('EMG-like')
-saveas(gcf,'EMG-like.png')
+features = ["Ch2__del__theta","Ch2__del__beta","Ch2__theta__beta","Ch53__del__theta","Ch53__del__beta","Ch53__theta__beta","EMG-like"];
+for i=1:7
+    subplot(2,2,1)
+    histogram(Features_1(:,i),'BinMethod','fd')
+    title('WAKE')
+    subplot(2,2,2)
+    histogram(Features_3(:,i),'BinMethod','fd')
+    title('NREM')
+    subplot(2,2,3)
+    histogram(Features_4(:,i),'BinMethod','fd')
+    title('NREM-REM')
+    subplot(2,2,4)
+    histogram(Features_5(:,i),'BinMethod','fd')
+    title('REM')
+    sgtitle(features(i))
+    saveas(gcf,features(i)+".png")
+end
