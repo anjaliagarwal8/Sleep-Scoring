f2532b6c767650c4ec7bf1862f42e8109fb1bfe5
Anjali Agarwal <anjaliagrl8@gmail.com>
2022-02-07 14:05:29 +0530

    Update StageDistribution.m

--- mcRBM_matlab/AnalysisScripts/StageDistribution.m
+++ mcRBM_matlab/AnalysisScripts/StageDistribution.m
@@ -1,24 +1,18 @@
-function [stageMat] = StageDistribution(uniqueStates,inferredStates)
+function [stageMat] = StageDistribution(uniqueStates,inferredStates,states)
 %% Compute each latent state's PDF according to how many epochs were manually 
 % labeled as Wakefulness, NREM, REM. This can be visualized with an RGB color shade.
 
 % computing the probability of each latent state to belong to each of the 3 sleep stages
 latentStates = size(uniqueStates,1);
-stageMat = zeros(latentStates,4);
+stageMat = zeros(latentStates,length(states.keys));
 
 for l=1:latentStates
     idx = find(inferredStates(:,1) == l);
     statePopulation = length(idx);
     
-    length_wake = length(find(inferredStates(idx,2)==1));
-    length_nrem = length(find(inferredStates(idx,2)==3));
-    length_nremtorem = length(find(inferredStates(idx,2)==4));
-    length_rem = length(find(inferredStates(idx,2)==5));
-    
-    stageMat(l,1) = length_wake;
-    stageMat(l,2) = length_nrem;
-    stageMat(l,3) = length_nremtorem;
-    stageMat(l,4) = length_rem;
+    for s=1:length(states.keys)
+        stageMat(l,s) = length(find(inferredStates(idx,2)==states.keys(s)));
+    end
 end
 
 [status, msg, msgID] = mkdir('StageDistribution');
@@ -35,7 +29,7 @@ aux_linkage = linkage(cdata,'average','euclidean');
 [H,T,outperm] = dendrogram(aux_linkage,0);
 
 heatmapfigure = figure('visible','off');
-heatmap({'WAKE','NREM','NREM-REM','REM'},outperm,cdata(outperm,:),'GridVisible','off');
+heatmap(states.names,outperm,cdata(outperm,:),'GridVisible','off');
 colormap 'turbo'
 ylabel('Latent States')
 title('HeatMap')
