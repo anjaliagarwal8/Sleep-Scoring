942f3ab53b6f5343687798bfa5f1120533d5497e
Anjali Agarwal <anjaliagrl8@gmail.com>
2022-02-07 16:20:32 +0530

    removed unnecessary files

--- RBM/compute_gradients.m
+++
@@ -1,9 +0,0 @@
-function [W_grad, vb_grad, hb_grad] = compute_gradients(v0, h0, vk, hk)
-%%
-% Computing the gradients for updating the weights
-% 
-    W_grad = (v0' * h0) - (vk' * hk);
-    vb_grad = (v0 - vk);
-    hb_grad = (h0 - hk);
-end
-
